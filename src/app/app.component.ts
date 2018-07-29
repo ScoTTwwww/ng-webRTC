bb77ae4af09823a7fe0840165c7ae92292ba20b6
Scott Ho <scott21739@gmail.com>
2018-07-29 20:13:03 +0800

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('videoElement') videoElement: any;  
+  video: any;
+
+  ngOnInit() {
+    this.video = this.videoElement.nativeElement;
+  }
+
+  start() {
+    this.initCamera({ video: true, audio: false });
+  }
+   sound() {
+    this.initCamera({ video: true, audio: true });
+  }
+  
+    initCamera(config:any) {
+    var browser = <any>navigator;
+
+    browser.getUserMedia = (browser.getUserMedia ||
+      browser.webkitGetUserMedia ||
+      browser.mozGetUserMedia ||
+      browser.msGetUserMedia);
+
+    browser.mediaDevices.getUserMedia(config).then(stream => {
+      this.video.src = window.URL.createObjectURL(stream);
+      this.video.play();
+    });
+  }
+}
