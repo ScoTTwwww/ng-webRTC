48b458916ae3c57d440ed82765f998a1ed1218a4
Scott Ho <scott21739@gmail.com>
2018-07-29 21:20:12 +0800

    初步有影像

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,46 @@
-import { Component } from '@angular/core';
-import { ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild,ElementRef  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('videoElement') videoElement: any;  
-  video: any;
+export class AppComponent implements AfterViewInit {
+  @ViewChild('videoElement') videoElement: ElementRef;  
+ 
+
+  
+  constructor(
+   
+
+) {
+    // local peer
+ 
+}
 
   ngOnInit() {
-    this.video = this.videoElement.nativeElement;
-
-    var video = document.querySelector('video')
-    
-            var constraints = {
-                audio: false,
-                video: {
-                    facingMode: 'user'
-                }
-            }
-    
-            navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-                video.srcObject = stream
-            })
+   const video =  this.videoElement.nativeElement
+    /* var video = document.querySelector('video')
+  */
+    var constraints = {
+      audio: true,
+      video: {
+          facingMode: 'user'
+      }
   }
 
-  start() {
-    this.initCamera({ video: true, audio: false });
-  }
-   sound() {
-    this.initCamera({ video: true, audio: true });
-  }
+  navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+    video.srcObject = stream
   
-    initCamera(config:any) {
-    var browser = <any>navigator;
-
-    browser.getUserMedia = (browser.getUserMedia ||
-      browser.webkitGetUserMedia ||
-      browser.mozGetUserMedia ||
-      browser.msGetUserMedia);
-
-    browser.mediaDevices.getUserMedia(config).then(stream => {
-      this.video.src = window.URL.createObjectURL(stream);
-      this.video.play();
-    });
+      
+  })
+ 
+          
   }
 
 
-
+  ngAfterViewInit(): void {
+    
+  }
+ 
 }
